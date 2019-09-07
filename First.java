4b19d6ba3329b434c03a4252a0df72b17fc670de
rakeshtraining <52368334+rakeshtraining@users.noreply.github.com>
2019-09-07 23:39:53 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.edureka.co");
+		driver.manage().window();
+		String x = driver.getTitle();
+		System.out.print(x);
+		
+		
+
+	}
+
+}
